d1b106068335b57d7f37728ef64947258a60e367
Lucia Skalinova <skalinovalucia@gmail.com>
2021-04-21 13:32:19 +0200

    doplneni css a dokonceni

--- hra.js
+++ hra.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const btnElm = document.querySelectorAll('.cell')
+
+let move = 'circle';
+
+btnElm.forEach(cell => {
+  cell.addEventListener('click', (e) => {
+    const cell = e.target
+    if (move !== 'circle') {
+      cell.classList.add('add-cross');
+      document.querySelector('.ikon').src = 'img/circle.svg';
+      move = 'circle'
+    } else {
+      cell.classList.add('add-circle');
+      document.querySelector('.ikon').src = 'img/cross.svg';
+      move = 'cross'
+    }
+    cell.disabled = true
+  }, {once: true})
+})
