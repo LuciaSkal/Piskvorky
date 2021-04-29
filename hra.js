047effa86bc6a41a8db7cc808c30c8661252283c
Lucia Skalinova <skalinovalucia@gmail.com>
2021-04-29 18:27:43 +0200

    uprava po code review

--- hra.js
+++ hra.js
@@ -10,10 +10,12 @@ btnElm.forEach(cell => {
     if (move !== 'circle') {
       cell.classList.add('add-cross');
       document.querySelector('.ikon').src = 'img/circle.svg';
+      document.querySelector('.ikon').alt = 'circle';
       move = 'circle'
     } else {
       cell.classList.add('add-circle');
       document.querySelector('.ikon').src = 'img/cross.svg';
+      document.querySelector('.ikon').alt = 'cross';
       move = 'cross'
     }
     cell.disabled = true
